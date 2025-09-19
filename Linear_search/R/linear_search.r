a8adb0e75f0f5ea48576a57cf21dacc087e8d3db
Anna <anmax2045@yandex.ru>
2025-09-19 13:36:30 +0300

    Update linear_search.r

--- Linear_search/R/linear_search.r
+++ Linear_search/R/linear_search.r
@@ -1,1 +1,10 @@
+arr <- c(123, 62, 52111, 9891, 1)
+target <- 1
+index <- which(arr == target)
 
+if (length(index) > 0) {
+    i <- index[1]
+    cat("The element", target, "was found at position", i, "\n")
+} else {
+    cat("The element", target, "was not found\n")
+}
